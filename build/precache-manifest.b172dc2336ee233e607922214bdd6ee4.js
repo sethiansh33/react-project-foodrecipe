db4647fa11755f41178a19976733439a12923c21
Ansh Sethi <ansh0167.cse19@chitkara.edu.in>
2021-06-02 03:53:31 +0530

    y

---
+++ build/precache-manifest.b172dc2336ee233e607922214bdd6ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe397f72bbd7697809d8a7d3a4816902",
+    "url": "/sethiansh33/react-project-foodrecipe/index.html"
+  },
+  {
+    "revision": "4f7d63da92f2410d2822",
+    "url": "/sethiansh33/react-project-foodrecipe/static/css/main.fdf8a849.chunk.css"
+  },
+  {
+    "revision": "188037c5abeaf672ef48",
+    "url": "/sethiansh33/react-project-foodrecipe/static/js/2.48b3b32d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sethiansh33/react-project-foodrecipe/static/js/2.48b3b32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7d63da92f2410d2822",
+    "url": "/sethiansh33/react-project-foodrecipe/static/js/main.9621e3f7.chunk.js"
+  },
+  {
+    "revision": "bf1a9fcf8358b27f622c",
+    "url": "/sethiansh33/react-project-foodrecipe/static/js/runtime-main.ca4f1a1e.js"
+  }
+]);
